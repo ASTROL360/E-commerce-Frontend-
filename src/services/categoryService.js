654de7dcd0c80b3f0606c933836e470005a00d55
LayiCode <olayiwolauthman310@gmail.com>
2026-08-17 19:13:59 +0100

    Update categoryService.js

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -2,7 +2,6 @@ import api from './api'
 
 const categoryService = {
   getAll: () => api.get('/categories'),
-  getById: (id) => api.get(`/categories/${id}`),
   create: (data) => api.post('/categories', data),
   update: (id, data) => api.put(`/categories/${id}`, data),
   remove: (id) => api.delete(`/categories/${id}`),
