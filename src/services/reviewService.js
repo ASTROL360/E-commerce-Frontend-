b042fc932a9df376e931c5dd38329767f87eb384
LayiCode <olayiwolauthman310@gmail.com>
2026-08-18 22:00:00 +0100

    Update reviewService.js

--- src/services/reviewService.js
+++ src/services/reviewService.js
@@ -2,7 +2,7 @@ import api from './api'
 
 const reviewService = {
   getByProduct: (productId, params = {}) => api.get(`/products/${productId}/reviews`, { params }),
-  create: (productId, data) => api.post(`/products/${productId}/reviews`, data),
+  create: (productId, data) => api.post('/reviews', { ...data, productId }),
   update: (reviewId, data) => api.put(`/reviews/${reviewId}`, data),
   remove: (reviewId) => api.delete(`/reviews/${reviewId}`),
 }
