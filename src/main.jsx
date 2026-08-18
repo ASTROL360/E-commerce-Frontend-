4ac520718bf3f4040d4d685ede264078e53e45e6
LayiCode <olayiwolauthman310@gmail.com>
2026-08-18 20:53:03 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import { CartProvider } from './contexts/CartContext'
+import { ToastProvider } from './contexts/ToastContext'
+import { WishlistProvider } from './contexts/WishlistContext'
 import ErrorBoundary from './components/common/ErrorBoundary'
 import App from './App'
 import './index.css'
@@ -12,9 +14,13 @@ createRoot(document.getElementById('root')).render(
     <ErrorBoundary>
       <BrowserRouter>
         <AuthProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
+          <ToastProvider>
+            <WishlistProvider>
+              <CartProvider>
+                <App />
+              </CartProvider>
+            </WishlistProvider>
+          </ToastProvider>
         </AuthProvider>
       </BrowserRouter>
     </ErrorBoundary>
